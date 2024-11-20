c9265d29cd34dc8a6434eb77bd3abeae30aba1c5
Juan Campa <juancampa@gmail.com>
2024-11-20 17:48:48 -0500

    Use correct api endpoint (#28)

--- src/vs/code/browser/workbench/membrane.ts
+++ src/vs/code/browser/workbench/membrane.ts
@@ -64,8 +64,7 @@ export async function membraneApi(
 	path: `/${string}`,
 	body?: BodyInit
 ): Promise<Response> {
-	// const isDev = window.location.hostname === 'localhost';
-	const baseUrl = 'https://staging.membrane.io';
+	const baseUrl = 'https://api.membrane.io';
 
 	const secretProvider = SecretStorageProvider.getInstance();
 	const token = await secretProvider.getAuthToken();
