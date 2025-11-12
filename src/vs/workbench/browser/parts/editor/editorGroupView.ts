c1ec6bc83408a17ba4516de209a992c5a4b1d21c
Juan Campa <juancampa@gmail.com>
2025-11-12 03:32:22 -0500

    Add inner shadow to vscode

--- src/vs/workbench/browser/parts/editor/editorGroupView.ts
+++ src/vs/workbench/browser/parts/editor/editorGroupView.ts
@@ -136,6 +136,10 @@ export class EditorGroupView extends Themable implements IEditorGroupView {
 	private readonly progressBar: ProgressBar;
 
 	private readonly editorContainer: HTMLElement;
+
+	// MEMBRANE: Inner shadow
+	private readonly innerShadow: HTMLElement;
+
 	private readonly editorPane: EditorPanes;
 
 	private readonly disposedEditorsWorker = this._register(new RunOnceWorker<EditorInput>(editors => this.handleDisposedEditors(editors), 0));
@@ -226,6 +230,11 @@ export class EditorGroupView extends Themable implements IEditorGroupView {
 			// Editor container
 			this.editorContainer = $('.editor-container');
 			this.element.appendChild(this.editorContainer);
+
+			// MEMBRANE: Inner shadow
+			this.innerShadow = document.createElement('div');
+			this.innerShadow.classList.add('membrane-inner-shadow');
+			this.element.appendChild(this.innerShadow);
 
 			// Editor pane
 			this.editorPane = this._register(this.scopedInstantiationService.createInstance(EditorPanes, this.element, this.editorContainer, this));
